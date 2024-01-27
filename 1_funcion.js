0371f3694d8714b79b1cf4cd22d5d2a37ad27b2e
maximilianoarevalo <maximiliano.arevalo@usach.cl>
2024-01-27 00:30:05 -0300

    Ejercicio 1 y 2 listos

---
+++ 1_funcion.js
@@ -0,0 +1,9 @@
+// Transformar esta declaración de función a una función de expresión.
+// function example(a, b, c){
+//    return a+b+c
+//}
+
+// Solución:
+let example = function(a, b, c){
+    return a+b+c;
+}
---
+++ 2_arrow.js
@@ -0,0 +1,7 @@
+// Transformar la siguiente función a una arrow function de una linea.
+// let suma = function(a, b){
+//    return a + b
+// }
+
+// Solución:
+let suma = (a, b) => {return a+b};
