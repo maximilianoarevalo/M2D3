0caf97bd4dce746887524b7ed2fe5de29223d878
maximilianoarevalo <maximiliano.arevalo@usach.cl>
2024-01-27 14:08:22 -0300

    3.2 y 3.3 terminado

--- assets/js/4_colores.js
+++ assets/js/4_colores.js
@@ -1,3 +1,5 @@
+// Se incluye todo en el mismo archivo, ya que los puntos pertenecen al punto 3
+// 3.1 y 3.2
 function paintBlack(divFromDOM){
     divFromDOM.style.backgroundColor = 'black';
 }
@@ -6,3 +8,24 @@ document.addEventListener("click", function(){
     paintBlack(div);
 });
 
+// 3.3
+let keyColor;
+
+function changeDivColor(keyColor){
+    const ele = document.getElementById('key');
+    ele.style.backgroundColor = keyColor;
+}
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'a') {
+        keyColor = 'pink';
+    } else if (event.key === 's') {
+        keyColor = 'orange';
+    } else if (event.key === 'd') {
+        keyColor = 'lightblue';
+    } else{
+        keyColor = null;
+    }
+    changeDivColor(keyColor);
+})
+    
